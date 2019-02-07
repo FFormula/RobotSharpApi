310102a7ba201c4772ec82e3ad79291a22f08f3a
Jevgenij Volosatov <fformula@gmail.com>
2019-02-07 11:23:32 -0800

    mvoe config files to own folder

---
+++ config/pdo.php.default
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'pdo' => [
+        'dsn' => 'mysql:host=localhost;dbname=ROBOT;charset=utf8',
+        'user' => 'root',
+        'pass' => ''
+    ]
+];
+
--- www/api.php
+++ www/api.php
@@ -3,7 +3,6 @@
 if (!file_exists('../vendor/autoload.php')) die('run "composer update"');
 include '../vendor/autoload.php';
 
-if (!file_exists('../config.php')) die('Copy "config.default.php" to "config.php" and edit it');
-$config = require '../config.php';
-$db = new FFormula\RobotSharp\Service\PdoDB($config['pdo']);
+if (!file_exists('../config/pdo.php')) die('Copy "config/pdo.php.default" to "config/pdo.php" and edit it');
+$db = new FFormula\RobotSharp\Service\PdoDB(require '../config/pdo.php');
 echo (new FFormula\RobotSharp\ApiSystem\Run())->start($_GET, $_POST);
